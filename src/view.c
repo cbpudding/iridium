9fa20c3d5623ffbe35c34422925d105d4abffbd2
Alexander Hill <ahill@breadpudding.dev>
2024-03-25 21:57:38 -0400

    Reverted to OpenGL 2.0 for compatibility

--- src/view.c
+++ src/view.c
@@ -10,9 +10,9 @@ void ir_view_drop(ir_view *view) {
 }
 
 int ir_view_new(ir_view *view) {
-    al_set_new_display_flags(ALLEGRO_FULLSCREEN | ALLEGRO_OPENGL_3_0);
-    al_set_new_display_option(ALLEGRO_OPENGL_MAJOR_VERSION, 3, ALLEGRO_REQUIRE);
-    al_set_new_display_option(ALLEGRO_OPENGL_MINOR_VERSION, 2, ALLEGRO_REQUIRE);
+    al_set_new_display_flags(ALLEGRO_FULLSCREEN | ALLEGRO_OPENGL);
+    al_set_new_display_option(ALLEGRO_OPENGL_MAJOR_VERSION, 2, ALLEGRO_REQUIRE);
+    al_set_new_display_option(ALLEGRO_OPENGL_MINOR_VERSION, 0, ALLEGRO_REQUIRE);
     al_set_new_display_option(ALLEGRO_DEFAULT_SHADER_PLATFORM, ALLEGRO_SHADER_GLSL, ALLEGRO_REQUIRE);
     al_set_new_display_option(ALLEGRO_VSYNC, 1, ALLEGRO_SUGGEST);
     al_set_new_window_title("Iridium");
