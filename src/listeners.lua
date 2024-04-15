2243f3c6689794f3d8bdda61534fccdd96317c29
Alexander Hill <ahill@breadpudding.dev>
2024-04-14 23:48:14 -0400

    Added more functional listeners

--- src/listeners.lua
+++ src/listeners.lua
@@ -2,6 +2,9 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+-- Utility Functions
+
+-- TODO: Replace ir.register with something a bit more declarative ~ahill
 function ir.register(handler)
     if type(handler) ~= "table" or #handler ~= 2 or type(handler[1]) ~= "number" or type(handler[2]) ~= "function" then
         ir.error("ir.register: Invalid subscription registration")
@@ -15,18 +18,35 @@ function ir.register(handler)
     return false
 end
 
+-- Event Listeners
+
 ir.listener = {}
 
-function ir.listener.keydown(pattern, msg)
-    return {
-        ir.internal.EVENT_KEY_DOWN,
-        function(event)
-            for k, v in pairs(pattern) do
-                if event[k] ~= v then
-                    return ir.msg.NOTHING
+function ir.listener.generic(kind)
+    return function(pattern, msg)
+        return {
+            kind,
+            function(event)
+                for k, v in pairs(pattern) do
+                    if type(v) == "function" then
+                        if not v(event[k]) then
+                            return ir.msg.NOTHING
+                        end
+                    elseif event[k] ~= v then
+                        return ir.msg.NOTHING
+                    end
                 end
+                return msg
             end
-            return msg
-        end
-    }
+        }
+    end
 end
+
+ir.listener.keychar = ir.listener.generic(ir.internal.EVENT_KEY_CHAR)
+ir.listener.keydown = ir.listener.generic(ir.internal.EVENT_KEY_DOWN)
+ir.listener.keyup = ir.listener.generic(ir.interval.EVENT_KEY_UP)
+ir.listener.mouseaxes = ir.listener.generic(ir.internal.EVENT_MOUSE_AXES)
+ir.listener.mousedown = ir.listener.generic(ir.internal.EVENT_MOUSE_DOWN)
+ir.listener.mouseenter = ir.listener.generic(ir.internal.EVENT_MOUSE_ENTER_DISPLAY)
+ir.listener.mouseleave = ir.listener.generic(ir.internal.EVENT_MOUSE_LEAVE_DISPLAY)
+ir.listener.mouseup = ir.listener.generic(ir.internal.EVENT_MOUSE_UP)
